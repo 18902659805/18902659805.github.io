39947dff4bd549608a2079bf7028a0876c1f02dd
18902659805 <426003742@qq.com>
2024-12-25 11:34:39 +0800

    Site updated: 2024-12-25 11:34:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/24/hello-world/"];function toRandomPost(){
+var posts=["2024/12/25/hello-world/","2024/12/25/产品-K2/","2024/12/25/产品-TP1/","2024/12/24/软著商标/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
